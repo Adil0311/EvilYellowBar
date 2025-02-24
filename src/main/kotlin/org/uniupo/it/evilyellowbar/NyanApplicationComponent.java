789658cd96bed840ce9ab4690f8701abe48d8eea
Adil11 <20045391@studenti.uniupo.it>
2025-02-24 21:59:35 +0100

    Modifiche

--- src/main/kotlin/org/uniupo/it/evilyellowbar/NyanApplicationComponent.java
+++ src/main/kotlin/org/uniupo/it/evilyellowbar/NyanApplicationComponent.java
@@ -1,4 +1,29 @@
 package org.uniupo.it.evilyellowbar;
 
-public class NyanApplicationComponent {
+import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.LafManagerListener;
+import com.intellij.openapi.application.ApplicationActivationListener;
+import com.intellij.openapi.wm.IdeFrame;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class NyanApplicationComponent implements LafManagerListener, ApplicationActivationListener {
+    public NyanApplicationComponent() {
+    }
+
+    @Override
+    public void lookAndFeelChanged(@NotNull LafManager lafManager) {
+        // see https://plugins.jetbrains.com/docs/intellij/plugin-listeners.html
+        updateProgressBarUi();
+    }
+
+    @Override
+    public void applicationActivated(@NotNull IdeFrame ideFrame) {
+        updateProgressBarUi();
+    }
+    private void updateProgressBarUi() {
+        UIManager.put("ProgressBarUI", NyanProgressBarUi.class.getName());
+        UIManager.getDefaults().put(NyanProgressBarUi.class.getName(), NyanProgressBarUi.class);
+    }
 }
